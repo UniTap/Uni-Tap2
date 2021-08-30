ebf0b48c44f6dd4117c4d25792e820eb320f6544
Mann-tech13 <mannshah.ict19@gmail.com>
2021-08-30 16:04:36 +0530

    Initial Login web page

---
+++ unitap-portal/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import LoginPage from './LoginPage';
+import { BrowserRouter, Route, Link } from "react-router-dom"
+
+function App() {
+  return (
+    <div className="App">
+      <LoginPage/>
+    </div>
+  );
+}
+
+export default App;
