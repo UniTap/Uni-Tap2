51e96753c7dbe046bc84f0bb0473526fa3b0c1d0
Mann-tech13 <mannshah.ict19@gmail.com>
2021-08-31 14:44:19 +0530

    Little modification

--- unitap-portal/src/App.js
+++ unitap-portal/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import LoginPage from './LoginPage';
 import { BrowserRouter, Route, Link } from "react-router-dom"
-import Glass from './Glass.js'
 
 function App() {
   return (
