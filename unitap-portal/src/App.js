cadce506b04321dcf1a305a74a67056a2332e8bd
Mann-tech13 <mannshah.ict19@gmail.com>
2021-09-09 17:02:56 +0530

    Route for profile page

--- unitap-portal/src/App.js
+++ unitap-portal/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import LoginPage from './LoginPage';
 import HomePage from './HomePage';
 import Noticepage from './Noticepage'
+import Profilepage from './Profile'
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom"
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" component={ LoginPage } exact/>
           <Route path="/HomePage" component={ HomePage } exact/>
           <Route path="/Notice" component={ Noticepage } exact/>
+          <Route path="/Profile" component={ Profilepage } exact/>
         </Switch>
       </Router>
       {/* <LoginPage/> */}
---
+++ unitap-portal/src/Profile.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Profile.css'
+
+function Profile() {
+  return (
+    <div>
+      <h1>Here's profile</h1>
+    </div>
+  )
+}
+
+export default Profile
