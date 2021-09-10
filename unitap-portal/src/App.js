e541a9ba03f11e3bcfaf127cebd3517e59e8d4a4
Mann-tech13 <mannshah.ict19@gmail.com>
2021-09-10 22:27:05 +0530

    Grievance page -- majority design done...need to modify just CSS

--- unitap-portal/src/App.js
+++ unitap-portal/src/App.js
@@ -3,6 +3,7 @@ import LoginPage from './LoginPage';
 import HomePage from './HomePage';
 import Noticepage from './Noticepage'
 import Profilepage from './Profile'
+import Grievance from './Grievance';
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom"
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Route path="/HomePage" component={ HomePage } exact/>
           <Route path="/Notice" component={ Noticepage } exact/>
           <Route path="/Profile" component={ Profilepage } exact/>
+          <Route path="/Grievance" component={ Grievance } exact/>
         </Switch>
       </Router>
       {/* <LoginPage/> */}
