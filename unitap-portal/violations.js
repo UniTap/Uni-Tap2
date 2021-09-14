59c468f25b0f799788fb770cfe1c845e2d001ce0
BHAVYA0034 <80756523+BHAVYA0034@users.noreply.github.com>
2021-09-14 21:11:27 +0530

    Violations

---
+++ unitap-portal/violations.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import $ from 'jquery'
+import "./violations.css"
+import Navbar from "./Navbar"
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom"
+
+
+
+
